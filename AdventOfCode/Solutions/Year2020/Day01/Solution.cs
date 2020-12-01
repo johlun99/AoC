1cc652bc5a0e626836d706ab86842113493f2c13
Johan lundgren <lundgren.johan99@gmail.com>
2020-12-01 06:18:38 +0100

    2020-Day01

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -7,19 +7,51 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day01 : ASolution
     {
-
+        private readonly List<int> input = new List<int>();
         public Day01() : base(01, 2020, "")
         {
+            string[] data = Input.SplitByNewline();
 
+            foreach (var row in data)
+                input.Add(Convert.ToInt32(row));
         }
 
         protected override string SolvePartOne()
         {
+            List<int> copy = new List<int>(input);
+
+            for (int i = 0; i < copy.Count; i++)
+            {
+                for (int j = i; j < copy.Count; j++)
+                {
+                    if (copy[i] + copy[j] == 2020)
+                    {
+                        return Convert.ToString(copy[i] * copy[j]);
+                    }
+                }
+            }
+            
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            List<int> copy = new List<int>(input);
+
+            for (int i = 0; i < copy.Count; i++)
+            {
+                for (int j = i; j < copy.Count; j++)
+                {
+                    for (int x = j; x < copy.Count; x++)
+                    {
+                        if (copy[i] + copy[j] + copy[x] == 2020)
+                        {
+                            return Convert.ToString(copy[i] * copy[j] * copy[x]);
+                        }
+                    }
+                }
+            }
+            
             return null;
         }
     }
