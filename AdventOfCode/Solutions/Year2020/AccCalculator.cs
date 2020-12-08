109ea56a3e186b9f1912cb31dda419996cd64e59
Johan Lundgren <lundgren.johan99@gmail.com>
2020-12-08 08:32:22 +0100

    Day8

---
+++ AdventOfCode/Solutions/Year2020/AccCalculator.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    public class AccCalculator
+    {
+        public static (int value, bool finished) RunInput(Dictionary<int, (string cmd, string op)> operations)
+        {
+            List<int> keysVisited = new List<int>();
+            int accumulator = 0;
+            
+            for (int i = 0; i < operations.Count; i++)
+            {
+                if (keysVisited.Contains(i))
+                    return (accumulator, false);
+
+                keysVisited.Add(i);
+
+                string cmd = operations[i].cmd;
+                string op = operations[i].op;
+
+                switch (cmd)
+                {
+                    case "nop":
+                        break;
+                    case "acc":
+                        if (op[0] == '+')
+                            accumulator += int.Parse(op.Substring(1));
+
+                        else if (op[0] == '-')
+                            accumulator -= int.Parse(op.Substring(1));
+
+                        else
+                            throw new Exception("Invalid Accumulator exception");
+                        break;
+                    case "jmp":
+                        if (op[0] == '+')
+                            i += int.Parse(op.Substring(1)) - 1;
+
+                        else if (op[0] == '-')
+                            i -= int.Parse(op.Substring(1)) + 1;
+
+                        else
+                            throw new Exception("Invalid Jmp operation");
+                        break;
+                }
+            }
+
+            return (accumulator, true);   
+        }
+
+        public static Dictionary<int, (string cmd, string op)> ModOperations(Dictionary<int, (string cmd, string op)> input, Dictionary<string, List<string>> changeables)
+        {
+            Dictionary<int, (string cmd, string op)> operations = new Dictionary<int, (string cmd, string op)>();
+
+            int accumulator = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                operations = new Dictionary<int, (string, string)>(input);
+                
+                string cmd = input[i].cmd;
+                string op = input[i].op;
+
+                bool foundPath = false;
+
+                if (changeables.ContainsKey(cmd))
+                {
+                    foreach (string changeable in changeables[cmd])
+                    {
+                        operations[i] = (changeable, op);
+
+                        if (RunInput(operations).finished)
+                        {
+                            foundPath = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (foundPath)
+                    break;
+            }
+
+            return operations;
+        }
+    }
+}
--- AdventOfCode/Solutions/Year2020/Day08/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day08/Solution.cs
@@ -7,20 +7,45 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day08 : ASolution
     {
+        private readonly Dictionary<int, (string cmd, string op)> input = new Dictionary<int, (string cmd, string op)>();
 
         public Day08() : base(08, 2020, "")
         {
+            string[] lines = Input.SplitByNewline();
 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string cmd = lines[i].Substring(0, 3);
+                string op = lines[i].Substring(4);
+                
+                input.Add(i, (cmd, op));
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int accumulator = AccCalculator.RunInput(input).value;
+            
+            return Convert.ToString(accumulator);
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            Dictionary<int, (string cmd, string op)> operations = new Dictionary<int, (string cmd, string op)>(input);
+            List<int> keysVisited = new List<int>();
+
+            int accumulator = 0;
+
+            Dictionary<string, List<string>> changeables = new Dictionary<string, List<string>>();
+            
+            changeables.Add("nop", new List<string>{"jmp"});
+            changeables.Add("jmp", new List<string>{"nop"});
+
+            operations = AccCalculator.ModOperations(operations, changeables);
+
+            accumulator = AccCalculator.RunInput(operations).value;
+
+            return Convert.ToString(accumulator);
         }
     }
 }
