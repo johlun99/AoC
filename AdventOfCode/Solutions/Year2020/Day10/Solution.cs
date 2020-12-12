473d5c304266900df90c0a1820e4f23b04ae7bf8
Johan Lundgren <lundgren.johan99@gmail.com>
2020-12-12 16:40:13 +0100

    Days 10 - 12

--- AdventOfCode/Solutions/Year2020/Day10/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day10/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,87 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day10 : ASolution
     {
+        private static List<int> input = new List<int>();
 
         public Day10() : base(10, 2020, "")
         {
+            string[] stringInput = Input.SplitByNewline();
 
+            foreach (string row in stringInput)
+            {
+                input.Add(int.Parse(row));
+            }
+            
+            input.Sort();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            (int singles, int tripples) values = GetDifference();
+            return Convert.ToString(values.singles * values.tripples);
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return Convert.ToString(FindArrangements(0));
+        }
+
+        private static (int singles, int tripples) GetDifference()
+        {
+            (int singles, int tripples) values = (1, 1);
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                if (input[i] + 1 == input[i + 1])
+                    values.singles++;
+                
+                else if (input[i] + 3 == input[i + 1])
+                    values.tripples++;
+            }
+            
+            return values;
+        }
+
+        private static List<int> TryToRemoveIndex(List<int> list)
+        {
+            for (int i = list.Count - 2; i > 0; i--)
+            {
+                if (i < list.Count - 2 && list[i + 2] - list[i] <= 2)
+                {
+                    list.RemoveAt(i + 1);
+                    return list;
+                }
+            }
+            
+            for (int i = list.Count - 2; i > 0; i--)
+            {
+                if (i < list.Count - 2 && list[i + 2] - list[i] <= 3)
+                {
+                    list.RemoveAt(i + 1);
+                    return list;
+                }
+            }
+
+            return new List<int>();
+        }
+
+        private long FindArrangements(int index)
+        {
+            Console.WriteLine(index);
+            
+            if (index == input.Count - 1)
+                return 1;
+
+            long total = 0;
+            int nextIndex = index + 1;
+
+            while (nextIndex < input.Count && input[nextIndex] - input[index] <= 3)
+            {
+                total += FindArrangements(nextIndex);
+                nextIndex += 1;
+            }
+
+            return total;
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,211 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day11 : ASolution
     {
+        private readonly List<(int x, int y)> _neighbours = new List<(int x, int y)>
+        {
+            (-1, 1), (0, 1), (1, 1),
+            (-1, 0),         (1, 0),
+            (-1, -1), (0, -1), (1, -1)
+        };
+
+        private List<string> _lines;
+        private List<Tile> _states;
+        
+        public enum Tile
+        {
+            Floor,
+            Empty,
+            Full
+        }
 
         public Day11() : base(11, 2020, "")
         {
-
+           _lines = new List<string>(Input.SplitByNewline());
         }
 
         protected override string SolvePartOne()
         {
+            var states = _lines.Select(x => x.Select(c => c switch
+            {
+                'L' => Tile.Empty,
+                '#' => Tile.Full,
+                '.' => Tile.Floor
+            }).ToList()).ToList();
+
+            int count = 0;
+            
+            while (true)
+            {
+                count++;
+                var oldState = states.Select(l => l.Select(x => x).ToList()).ToList();
+                var newState = CheckRules(states);
+                var same = true;
+                for (int y = 0; y < newState.Count(); y++)
+                {
+                    for (int x = 0; x < newState.First().Count(); x++)
+                    {
+                        if (newState[y][x] != oldState[y][x])
+                        {
+                            same = false;
+                        }
+                    }
+                }
+
+                if (same)
+                {
+                    return Convert.ToString(newState.Sum(l => l.Count(x => x == Tile.Full)));
+                }
+
+                states = newState;
+            }
+            
             return null;
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var states = _lines.Select(x => x.Select(c => c switch
+            {
+                'L' => Tile.Empty,
+                '#' => Tile.Full,
+                '.' => Tile.Floor
+            }).ToList()).ToList();
+
+            int count = 0;
+            
+            while (true)
+            {
+                count++;
+                var oldState = states.Select(l => l.Select(x => x).ToList()).ToList();
+                var newState = CheckUpdatedRules(states);
+                var same = true;
+                for (int y = 0; y < newState.Count(); y++)
+                {
+                    for (int x = 0; x < newState.First().Count(); x++)
+                    {
+                        if (newState[y][x] != oldState[y][x])
+                        {
+                            same = false;
+                        }
+                    }
+                }
+
+                if (same)
+                {
+                    return Convert.ToString(newState.Sum(l => l.Count(x => x == Tile.Full)));
+                }
+
+                states = newState;
+            }
+        }
+        
+        public static List<List<Tile>> CheckRules(List<List<Tile>> lines)
+        {
+            var newState = lines.Select(l => l.Select(c => c).ToList()).ToList();
+
+            for (int y = 0; y < lines.Count(); y++)
+            {
+                var line = lines[y];
+                for (int x = 0; x < lines.First().Count(); x++)
+                {
+                    var upAllowed = y > 0;
+                    var downAllowed = y <= lines.Count()-2;
+                    var leftAllowed = x > 0;
+                    var rightAllowed = x <= line.Count()-2;
+
+                    var up = upAllowed && lines[y - 1][x] == Tile.Full;
+                    var down = downAllowed && lines[y + 1][x] == Tile.Full;
+                    var left = leftAllowed && lines[y][x-1] == Tile.Full;
+                    var right = rightAllowed && lines[y][x+1] == Tile.Full;
+
+                    var upLeft = upAllowed && leftAllowed && lines[y - 1][x - 1] == Tile.Full;
+                    var upRight = upAllowed && rightAllowed && lines[y - 1][x + 1] == Tile.Full;
+                    var downLeft = downAllowed && leftAllowed && lines[y + 1][x - 1] == Tile.Full;
+                    var downRight = downAllowed && rightAllowed && lines[y + 1][x + 1] == Tile.Full;
+
+                    var neighbours = new [] {up, down, left, right, upLeft, upRight, downLeft, downRight};
+                    
+                    var existing = newState[y][x];
+                    var all = neighbours.All(x => x);
+                    var adjacent = neighbours.Count(x => x);
+                    var none = neighbours.All(x => !x);
+
+                    if (existing == Tile.Full && adjacent >= 4)
+                    {
+                        newState[y][x] = Tile.Empty;
+                    } else if (existing == Tile.Empty && none)
+                    {
+                        newState[y][x] = Tile.Full;
+                    };
+                }
+            }
+
+            return newState;
+        }
+        
+        private static List<List<Tile>> CheckUpdatedRules(List<List<Tile>> lines)
+        {
+            var newState = lines.Select(l => l.Select(c => c).ToList()).ToList();
+
+            
+            for (int y = 0; y < lines.Count(); y++)
+            {
+                var line = lines[y];
+                for (int x = 0; x < lines.First().Count(); x++)
+                {
+                    Tile FirstSeat(int xdir, int ydir)
+                    {
+                        var xi = x + xdir;
+                        var yi = y + ydir;
+
+                        bool OOB()
+                        {
+                            return xi < 0 || yi < 0 || xi >= lines.First().Count() || yi >= lines.Count();
+                        }
+                        
+                        while (!OOB())
+                        {
+                            var spot = lines[yi][xi];
+                            if (spot != Tile.Floor)
+                            {
+                                return spot;
+                            }
+
+                            xi += xdir;
+                            yi += ydir;
+                        }
+
+                        return Tile.Floor;
+                    }
+                    
+                    var up = FirstSeat(0, -1) == Tile.Full;
+                    var down =  FirstSeat(0, 1) == Tile.Full;
+                    var left =  FirstSeat(-1, 0) == Tile.Full;
+                    var right =  FirstSeat(1, 0) == Tile.Full;
+
+                    var upLeft =  FirstSeat(-1, -1) == Tile.Full;
+                    var upRight =  FirstSeat(1, -1) == Tile.Full;
+                    var downLeft =  FirstSeat(-1, 1) == Tile.Full;
+                    var downRight =  FirstSeat(1, 1) == Tile.Full;
+
+                    var neighbours = new [] {up, down, left, right, upLeft, upRight, downLeft, downRight};
+                    
+                    var existing = newState[y][x];
+                    var all = neighbours.All(x => x);
+                    var adjacent = neighbours.Count(x => x);
+                    var none = neighbours.All(x => !x);
+
+                    if (existing == Tile.Full && adjacent >= 5)
+                    {
+                        newState[y][x] = Tile.Empty;
+                    } else if (existing == Tile.Empty && none)
+                    {
+                        newState[y][x] = Tile.Full;
+                    };
+                }
+            }
+
+            return newState;
         }
     }
 }
