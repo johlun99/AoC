9ab8b817a971fdbf1bcc826299902452c0a949af
Johan Lundgren <lundgren.johan99@gmail.com>
2021-11-30 21:55:10 +0100

    Prepared for 2021

---
+++ AdventOfCode/Solutions/Year2021/Day07/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Solutions.Year2021
+{
+
+    class Day07 : ASolution
+    {
+        public Day07() : base(07, 2021, "")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Year2021/Day09/Solution.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AdventOfCode.Solutions.Year2021
+{
+
+    class Day09 : ASolution
+    {
+        public Day09() : base(09, 2021, "")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
