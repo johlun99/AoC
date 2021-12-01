98313f5fc986ea0ceed4b607bdddee6ded9cabd3
Johan Lundgren <lundgren.johan99@gmail.com>
2021-12-01 06:51:09 +0100

    Day 1-2021

--- AdventOfCode/Solutions/Year2021/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day01/Solution.cs
@@ -1,25 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AdventOfCode.Solutions.Year2021
 {
 
     class Day01 : ASolution
     {
+        List<int> input = new List<int>();
         public Day01() : base(01, 2021, "")
         {
+            string[] data = Input.SplitByNewline();
 
+            foreach (var row in data)
+                input.Add(Convert.ToInt32(row));
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int countIncreases = 0;
+
+            for (int i = 1; i < input.Count; i++)
+                if (input[i] > input[i - 1])
+                    countIncreases++;
+
+            return countIncreases.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int countIncreases = 0;
+
+            for (int i = 0; i < input.Count - 3; i++) {
+                int a = input[i] + input[i + 1] + input[i + 2];
+                int b = input[i + 1] + input[i + 2] + input[i + 3];
+
+                if (a < b) 
+                    countIncreases++;
+            }
+
+            return countIncreases.ToString();
         }
     }
 }
